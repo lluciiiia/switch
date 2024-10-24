19f118425db25e4ff62602f961173b0435a27c46
binh.nguyento <nguyentobinh12@gmail.com>
2024-10-25 00:10:01 +0700

    feat: add checkin pages

--- src/app/components/SideLeft/SideLeft.tsx
+++ src/app/components/SideLeft/SideLeft.tsx
@@ -1,23 +1,54 @@
+import Image from "next/image";
+
 const SideLeft = () => {
+  const todos = [
+    { id: 1, text: "Confirm customer reservations" },
+    { id: 2, text: "Prepare check-in documents" },
+    { id: 3, text: "Prepare room key" },
+    { id: 4, text: "Clean checked-out rooms" },
+  ];
+
   return (
-    <div className="bg-gray-100 p-5 h-screen flex flex-col items-center gap-10">
+    <div className="bg-[#FE856F] p-5 h-full flex flex-col items-center gap-7 text-white">
       <div className="text-center">
-        <div className="w-24 h-8 bg-gray-300 rounded-md"></div>
-      </div>
-      <div className="text-center">
-        <div className="w-20 h-20 bg-gray-300 rounded-full mb-2"></div>
-        <p>Profile</p>
-      </div>
-      <div className="border-b-2 border-gray-300 w-full"></div>
-      <div className="text-center">
-        <div className="w-36 h-36 border-2 border-gray-300 rounded-full mb-2 flex justify-center items-center">
-          <p>Tasks Percent</p>
+        <div>
+          <Image src="/images/lyfdesk.png" width={80} height={80} alt="logo" />
         </div>
       </div>
-      <div className="border-b-2 border-gray-300 w-full"></div>
       <div className="text-center">
-        <div className="w-52 h-36 bg-gray-300 rounded-md flex justify-center items-center">
-          <p>To Do List</p>
+        <div>
+          <Image
+            src="/images/profile.png"
+            width={75}
+            height={75}
+            alt="arrow"
+            className="object-cover w-full h-full rounded-full"
+          />
+        </div>
+        <p className="font-bold my-2">Hyejin Kim</p>
+        <p className="text-sm">Front Office</p>
+      </div>
+      <div className="border-b-2 border-white w-full"></div>
+      <div className="text-center">
+        <div className="w-36 h-36 border-2 border-white rounded-full mb-2 flex justify-center items-center flex-col">
+          <p className="text-5xl">54</p>
+          <p>Percent</p>
+        </div>
+      </div>
+      <div className="border-b-2 border-white w-full"></div>
+      <div className="text-center">
+        <div className="w-60 h-36 bg-white rounded-md flex justify-center items-center">
+          <ul className="text-sm text-black">
+            {todos.map((todo) => (
+              <li key={todo.id} className="flex items-center mb-2">
+                <input
+                  type="checkbox"
+                  className="mr-2 h-4 w-4 rounded border-gray-300"
+                />
+                <span>{todo.text}</span>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
